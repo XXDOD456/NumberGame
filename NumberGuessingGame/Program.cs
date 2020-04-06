4a09cf58d8853fe143d459080171c74b495bbbf3
XXDOD456 <kylemcconnell94@gmail.com>
2020-04-06 09:54:12 -0500

    finished

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my game!");
+            Console.WriteLine("Can you guess my favorite number?");
+            int num = 362;
+            var userInput  = int.Parse(Console.ReadLine());
+            if (num < userInput)
+            {
+                Console.WriteLine("Sorry, too high!");
+            }
+            else if (num > userInput)
+            {
+                Console.WriteLine("Sorry, too low!");
+            }
+            else
+            {
+                Console.WriteLine("That's right!");
+            }
+        }
+    }
+}
